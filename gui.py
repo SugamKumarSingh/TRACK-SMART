40cedefce6e020cf7e090f7d5308e1a1f5402f63
Sugam Kumar Singh <72367458+SugamKumarSingh@users.noreply.github.com>
2020-12-10 23:35:59 +0530

    Update gui.py
    added comments after each function for easy understanding while compiling

--- gui.py
+++ gui.py
@@ -1,90 +1,48 @@
 import tkinter as tk
 
 HEIGHT = 500
-WIDTH = 600
+WIDTH = 700
 
+#this function is just an exaample to explain how to use assign function to buttons
 def test_function(name):
 	print("This is the entry:", name)
-
-
 
 root = tk.Tk()
 root.title('SIGN UP for TRACK SMART Attendence')
 
+#this to define canvas in GUI
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="white")
 canvas.pack()
 
-
-
+#here i have added frame to our GUI for name entry
 frame = tk.Frame(root, bg='white', bd=10)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
+#to print message above both entries
 name_msg = tk.Message(frame, text='Enter your name')
 name_msg.config(bg='lightgreen', font=('times', 24, 'italic'))
 name_msg.place(relwidth=0.65, relheight=1)
 
-
+#entry field for name
 name = tk.Entry(frame, font=40)
 name.place(relwidth=0.65, relheight=1)
 
+#button for name
 name_button = tk.Button(frame, text="SUBMIT Name", font=40)
 name_button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 
-
-
+#creating lower frame for email entry
 lower_frame = tk.Frame(root, bg='white', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.1, anchor='n')
 
+#entry field for email
 email = tk.Entry(lower_frame, font=40)
 email.place(relwidth=0.65, relheight=1)
 
+#button for email
 email_button = tk.Button(lower_frame, text="SUBMIT Email", font=40)
 email_button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 
-
-
 root.mainloop()
-
-
-
-
-
-
-
-"""
-import tkinter as tk
-from tkinter import filedialog, Text
-import os
-
-root = tk.Tk()
-
-def signup():
-    print("SIGN UP")
-    pass
-
-def signin():
-    print("SIGN IN")
-    pass
-
-
-
-
-canvas = tk.Canvas(root, height=500, width=700, bg='#ff4800')
-canvas.pack()
-
-frame = tk.Frame(root, bg='#9d00ff')
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-new_student = tk.Button(frame, text='NEW STUDENT', activeforeground='red', padx=24, pady=10, fg='black', bg='gray', command=signup)
-new_student.place(x=200, y=150)
-
-existing_student = tk.Button(frame, text='EXISTING STUDENT', padx=10, pady=10, fg='black', bg='blue', command=signin)
-existing_student.place(x=200, y=250)
-
-
-
-
-root.mainloop()
-"""
