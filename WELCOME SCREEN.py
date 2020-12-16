d7fdb60ca5ae52a65d910e4eacba60934ea5faa9
Sugam Kumar Singh <72367458+SugamKumarSingh@users.noreply.github.com>
2020-12-16 15:22:45 +0530

    vr icon update
    added vr_icon.png and updated signup gui

--- WELCOME SCREEN.py
+++ WELCOME SCREEN.py
@@ -10,27 +10,33 @@ root.title('TRACK SMART Attendence System')
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="lightgreen")
 canvas.pack()
 
+
+#upper frame for heading
+upper_frame = tk.Frame(root, bg='lightgreen', bd=10)
+upper_frame.place(relx=0.5, relwidth=1, relheight=0.3, anchor='n')
+
 #to print welcome message
-name_msg = tk.Message(canvas, text='WELCOME TO SMART TRACK ATTENDENCE SYSTEM')
+name_msg = tk.Message(upper_frame, text='WELCOME TO SMART TRACK ATTENDENCE SYSTEM')
 name_msg.config(bg='lightgreen', font=('times', 24, 'italic'))
-name_msg.place(relwidth=0.4, relheight=1)
+name_msg.place(relwidth=1, relheight=1)
+
 
 #here i have added frame to our GUI for new user
 frame = tk.Frame(root, bg='lightgreen', bd=10)
-frame.place(relx=0.4, rely=0.3, relwidth=0.75, relheight=0.1, anchor='n')
+frame.place(relx=0.75, rely=0.4, relwidth=0.75, relheight=0.1, anchor='n')
 
 #button for new user
 sign_up = tk.Button(frame, text="SIGN UP", font=40)
-sign_up.place(relx=0.7, relheight=1, relwidth=0.3)
+sign_up.place(relheight=1, relwidth=0.3)
 
 
 #creating lower frame for email entry
 lower_frame = tk.Frame(root, bg='lightgreen', bd=10)
-lower_frame.place(relx=0.4, rely=0.5, relwidth=0.75, relheight=0.1, anchor='n')
+lower_frame.place(relx=0.75, rely=0.6, relwidth=0.75, relheight=0.1, anchor='n')
 
 #button for existing student
 sign_in = tk.Button(lower_frame, text="SIGN IN", font=40)
-sign_in.place(relx=0.7, relheight=1, relwidth=0.3)
+sign_in.place(relheight=1, relwidth=0.3)
 
 
 root.mainloop()
--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
-HEIGHT = 500
-WIDTH = 700
+HEIGHT = 2048
+WIDTH = 2048
 
 #this function is just an exaample to explain how to use assign function to buttons
 def test_function(name):
@@ -14,30 +14,27 @@ root.title('SIGN UP for TRACK SMART Attendence')
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="lightyellow")
 canvas.pack()
 
+
 #here i have added frame to our GUI for name entry
 frame = tk.Frame(root, bg='lightyellow', bd=10)
-frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.1, anchor='n')
+frame.place(relx=0.5, rely=0.25, relwidth=0.8, relheight=0.1, anchor='n')
 
 #entry field for name
-name = tk.Entry(frame, font=40)
-name.place(relwidth=0.65, relheight=1)
+name = tk.Entry(frame, font=('times', 36))
+name.place(relwidth=0.6, relheight=1)
+
+#photoimage for icon
+vr_image = tk.PhotoImage(file = "vr_icon.png")
+vr_icon = vr_image.subsample(11,11)
+
+#button for voice recognition
+vr_button = tk.Button(frame, text="Voice Recognistion", image = vr_icon, font=('times', 36))
+vr_button.place(relx=0.64, relheight=1, relwidth=0.07)
 
 #button for name
-name_button = tk.Button(frame, text="SUBMIT Name", font=40)
-name_button.place(relx=0.7, relheight=1, relwidth=0.3)
+name_button = tk.Button(frame, text="SUBMIT Name", font=('times', 36))
+name_button.place(relx=0.75, relheight=1, relwidth=0.25)
 
-
-#creating lower frame for email entry
-lower_frame = tk.Frame(root, bg='lightyellow', bd=10)
-lower_frame.place(relx=0.5, rely=0.5, relwidth=0.75, relheight=0.1, anchor='n')
-
-#entry field for email
-email = tk.Entry(lower_frame, font=40)
-email.place(relwidth=0.65, relheight=1)
-
-#button for email
-email_button = tk.Button(lower_frame, text="SUBMIT Email", font=40)
-email_button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 
 root.mainloop()
