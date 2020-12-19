9b7dac9547d13b161c7279b6af1360dd9e17c1a4
Sugam Kumar Singh <72367458+SugamKumarSingh@users.noreply.github.com>
2020-12-19 13:50:57 +0530

    Create dateAndTimeTutorial.py

---
+++ dateAndTimeTutorial.py
@@ -0,0 +1,12 @@
+import datetime as dt
+
+dateTime = dt.datetime.now()
+
+print(dateTime)
+print(dateTime.year)
+print(dateTime.month)
+print(dateTime.day)
+print(dateTime.hour)
+print(dateTime.minute)
+print(dateTime.second)
+print(dateTime.microsecond)
